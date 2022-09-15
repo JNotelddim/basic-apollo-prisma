342e3600e47b404a3c914ee4b535fa0ca355bf5f
JNotelddim <JNotelddim@users.noreply.github.com>
2022-09-15 10:28:30 -0700

    Add usersQuery
    [nexus isn't generating..]

--- graphql/User.ts
+++ graphql/User.ts
@@ -1,4 +1,4 @@
-import { objectType } from "nexus";
+import { extendType, objectType } from "nexus";
 
 export const User = objectType({
     name: "User",
@@ -6,5 +6,18 @@ export const User = objectType({
         t.nonNull.int("id");
         t.nonNull.string("email");
         t.string("name");
+    },
+});
+
+export const UsersQuery = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.list.nonNull.field("users", {
+            type: "User",
+            resolve: async (parent, args, context, info) => {
+                const res = await context.prisma.user.findMany();
+                return res;
+            },
+        });
     },
 });
